dff7ed53aaa85cce2ebbb1162e5e87fd52f7d50b
shtolce <kimdimka@yandex.ru>
2019-06-19 00:26:40 +1000

    error find

--- App/repository.model.ts
+++ App/repository.model.ts
@@ -5,10 +5,18 @@ export class Model{
     private products:Product[];
     private locator=(p:Product,id:number)=>p.id==id;
     constructor(){
-        
-
-
+        this.dataSource=new SimpleDataSource();
+        this.products=new Array<Product>();
+        this.dataSource.getData().forEach(p=>this.products.push(p));
     }
+    getProducts():Product[]{
+        return this.products;
+    }
+    getProduct(id :number):Product{
+        this.products.
+        return this.products.find(p=>this.locator(p,id));
+    }
+
 
 
 }
